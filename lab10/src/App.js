68225f97ceb220d0a1ddcb80422d3f52252c0176
RileySamos <155810341+RileySamos@users.noreply.github.com>
2024-05-15 07:29:44 +0000

    lab10

--- lab10/src/App.js
+++ lab10/src/App.js
@@ -2,29 +2,17 @@ import f3 from './images/f3.png';
 import f4 from './images/f4.png';
 import f5 from './images/f5.png';
 import './App.css';
- //не работало как обычно (с указанием папки)
+import ToDo from './components/ToDo';
 function App() {
   return (
-      <div className="container">
-          <h2 className="main_title">Что будет по плану?</h2>
-          <div className="main_block">
-              <div className="blocks">
-                  <img src={f3} alt="pic"/>
-                      <h3>Обучение</h3>
-                      <p>Почему бы вам не научиться создавать сайты наподобии этого?</p>
-              </div>
-              <div className="blocks">
-                  <img className="imagen" src={f4} alt="pic"/>
-                      <h3>Возможности</h3>
-                      <p>Путь к идеалу долгий, но можно хотя бы приблизится к нему.</p>
-              </div>
-              <div className="blocks">
-                  <img src={f5} alt="pic"/>
-                      <h3>Успех</h3>
-                      <p>Мы гарантируем, что вы сумеете трудоустроиться в высшие компании.</p>
-              </div>
-          </div>
-      </div>
+    <div className="container">
+    <h2 className="main_title">Что будет по плану?</h2>
+        <div className="main_block">
+        <ToDo title="Обучение" img={f3} text='Почему бы вам не научиться создавать сайты наподобии этого?'/>
+        <ToDo title="Возможности" img={f4} text='Путь к идеалу долгий, но можно хотя бы приблизится к нему.'/>
+        <ToDo title="Успех" img={f5} text='Мы гарантируем, что вы сумеете трудоустроиться в высшие компании.'/>
+        </div>
+    </div>
   );
 }
 
