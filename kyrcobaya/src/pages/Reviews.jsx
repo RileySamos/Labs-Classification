a151d7cfa17cc4a547204d06e91e18c071b82eac
RileySamos <155810341+RileySamos@users.noreply.github.com>
2024-06-04 17:23:04 +0500

    Delete kyrcobaya directory

--- kyrcobaya/src/pages/Reviews.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Reviews() {
-  return (
-    <div>Я являюсь вашим студентом, Максим Александрович. А ещё идиот (судя по словам учителей в школе).</div>
-  )
-}
-
-export default Reviews
--- kyrcobaya/src/pages/SiFi.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SiFi() {
-  return (
-    <div>Я являюсь вашим студентом, Максим Александрович. А ещё идиот (судя по словам учителей в школе).</div>
-  )
-}
-
-export default SiFi
